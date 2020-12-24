e1c7cbf971c25e1655216be8555792ae44dd568b
MuXitest <dulijuan6666@163.com>
2020-12-24 17:15:38 +0800

    课后作业：加减乘除测试用例

--- pythoncode/test_calc.py
+++ pythoncode/test_calc.py
@@ -16,9 +16,25 @@ def get_datas():
         div_ids = datas["divmyids"]
         return [add_datas,add_ids,sub_datas,sub_ids,mul_datas,mul_ids,div_datas,div_ids]
 
+def setup_module():
+    print("\n整个test_calc.py模块开始前只执行一次")
+def teardown_module():
+    print("\n整个test_calc.py模块结束后只执行一次")
+
+
 class TestCalc:
+
     def setup_class(self):
         self.calc = Calculator()
+        print("\n所有用例执行前")
+    def teardown_class(self):
+        print("\n所有用例结束执行")
+
+    def setup_methond(self):
+        print("每个用例开始执行计算之前打印")
+    def teardown_method(self):
+        print("每个用例开始计算之后打印")
+
     @pytest.mark.parametrize("a,b,expect",get_datas()[0],ids=get_datas()[1])
     def test_add(self,a,b,expect):
         result = self.calc.add(a,b)
